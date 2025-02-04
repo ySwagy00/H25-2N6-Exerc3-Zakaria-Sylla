7d3c837a5d2620a24417f79844a14def0a8ae04a
ySwagy00 <6256003@cegepmontpetit.ca>
2025-02-04 16:56:36 -0500

    Ex3 fini (manque juste rename)

--- Ex3 Renommer plusieurs fichiers.py
+++ Ex3 Renommer plusieurs fichiers.py
@@ -6,3 +6,14 @@
 # #     utilisez strip pour enlever les espaces qui pourraient rester pour le titre et le numéro
 # #     en plus utilisez zfill pour remplir le numéro de sorte que 1 deviendra 01
 # #     recréez le nouveau nom de fichier#   utliser os.rename pour renommer le fichier
+
+import os
+os.chdir(os.path.dirname(__file__))
+os.chdir("Ex3 Videos")
+for fichier in os.listdir("C:\\Users\\6256003\OneDrive - Cégep Édouard-Montpetit\\Prog2\\R03 Exercices Depart\\Ex3 Videos"):
+    nom, ext = os.path.splitext(fichier)
+    titre, cours, numero = nom.split("_")
+    nouveau_nom = f"{titre.strip()}-{cours.strip()}-{numero.strip().zfill(2)}{ext}"
+    
+   
+
